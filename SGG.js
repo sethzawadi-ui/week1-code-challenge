41b689351872ccaad8f74321ca3849ad66f0ee6c
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-09-09 14:47:55 +0300

    created a student grade generator using html and javascript

---
+++ SGG.js
@@ -0,0 +1,41 @@
+function getGrade()
+// the function is called when the user clicks the "Get Grade" button
+{
+let marks = document.getElementById("marks").value
+// Sends a prompt to the user to ask the user to input their marks
+let outputMessage;
+if (marks < 0 || marks > 100) {
+    outputMessage = 'Invalid input. Please enter a number between 0 and 100'; //holds the final text to be displayed
+    // if the number input is not a number or not between 0 and 100, print this message
+} else {
+  // If input is valid, we prepare to assign a grade.
+  let grade;
+
+  // Check the grading conditions:
+  if (marks > 79) {
+    grade = "A";
+    // If marks are greater than 79, grade is "A".
+  } 
+  else if (marks >= 60) {
+    grade = "B";
+    // If marks are between 60 and 79, grade is "B".
+  } 
+  else if (marks >= 49) {
+    grade = "C";
+     // If marks are between 49 and 59, grade is "C".
+  } 
+  else if (marks >= 40) {
+    grade = "D";
+     // If marks are between 40 and 49, grade is "D".
+  } 
+  else {
+    grade = "E";
+    // If marks are less than 40, grade is "E".
+  }
+
+  outputMessage = `The grade is: ${grade}`;
+  // `${grade}` inserts the grade value into the string.
+}
+document.getElementById("output").innerHTML = outputMessage;
+// // This sends the result to the <p id="output"> in the HTML
+}
